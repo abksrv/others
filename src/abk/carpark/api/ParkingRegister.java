9b3f1d41b6147bddaa23f63a214186e6b22b2a81
z003cp8w <z003cp8w@AAEINGN546826D>
2017-01-23 16:25:22 +0530

    Implemented parking register, changed impl a little bit

--- src/abk/carpark/api/ParkingRegister.java
+++ src/abk/carpark/api/ParkingRegister.java
@@ -10,42 +10,70 @@ import java.util.Set;
 
 import abk.carpark.util.SlotRegNumBiMap;
 
-public class ParkingRegister {
+public class ParkingRegister
+{
+    
+    SlotRegNumBiMap regNumSlotbiMap = new SlotRegNumBiMap();
+    Map<Integer, String> colorToSlot = new HashMap<>();
+    
+    public ParkingRegister()
+    {
+    }
+    
+    public ParkingEntry[] status()
+    {
+        ParkingEntry[] entries = new ParkingEntry[regNumSlotbiMap.size()];
+        Set<Entry<Integer, String>> entrySet = colorToSlot.entrySet();
+        int i = 0;
+        for (Entry<Integer, String> e : entrySet)
+        {
+            Integer slot = e.getKey();
+            String color = e.getValue();
+            entries[i++] = new ParkingEntry(slot, regNumSlotbiMap.getBySlot(slot),
+                color);
+        }
+        Arrays.sort(entries);
+        return entries;
+    }
+    
+    public void enter(int slot, String regNum, String color)
+    {
+        regNumSlotbiMap.put(slot, regNum);
+        colorToSlot.put(slot, color);
+    }
+    
+    public void exit(int slot)
+    {
+        if (regNumSlotbiMap.getBySlot(slot) == null)
+            throw new IllegalArgumentException("No such entry!");
+        regNumSlotbiMap.remove(slot);
+        colorToSlot.remove(slot);
+    }
 
-	SlotRegNumBiMap biMap = new SlotRegNumBiMap();
-	Map<String, List<Integer>> colorToSlot = new HashMap<>();
+    public List<String> getRegNumsByColor(String string)
+    {
+        List<String> list = new ArrayList<>();
+        for(Entry<Integer, String> entry:colorToSlot.entrySet())
+        {
+            if(entry.getValue().equals(string))
+                list.add(regNumSlotbiMap.getBySlot(entry.getKey()));
+        }
+        return list;
+    }
 
-	public ParkingRegister() {
-	}
+    public List<Integer> getSlotByColor(String string)
+    {
+        List<Integer> list = new ArrayList<>();
+        for(Entry<Integer, String> entry:colorToSlot.entrySet())
+        {
+            if(entry.getValue().equals(string))
+                list.add(entry.getKey());
+        }
+        return list;
+    }
 
-	public ParkingEntry[] status() {
-		ParkingEntry[] entries = new ParkingEntry[biMap.size()];
-		Set<Entry<String, List<Integer>>> entrySet = colorToSlot.entrySet();
-		int i = 0;
-		for (Entry<String, List<Integer>> e : entrySet) {
-			String color = e.getKey();
-			for (Integer slot : e.getValue()) {
-				entries[i++] = new ParkingEntry(slot, biMap.getBySlot(slot),
-						color);
-			}
-		}
-		Arrays.sort(entries);
-		return entries;
-	}
-
-	public void enter(int slot, String regNum, String color) {
-		biMap.put(slot, regNum);
-		List<Integer> list = colorToSlot.get(color);
-		if (list == null) {
-			list = new ArrayList<Integer>();
-			colorToSlot.put(color, list);
-		}
-		list.add(slot);
-	}
-
-	public void exit(int i) {
-		if (biMap.getBySlot(i) == null)
-			throw new IllegalArgumentException("No such entry!");
-		biMap.remove(i);
-	}
+    public int getSlotByRegNum(String string)
+    {
+        return regNumSlotbiMap.getByReg(string);
+    }
 }
--- test/abk/carpark/api/ParkingRegisterTest.java
+++ test/abk/carpark/api/ParkingRegisterTest.java
@@ -1,4 +1,7 @@
+package abk.carpark.api;
+
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,50 +12,154 @@ import org.junit.rules.ExpectedException;
 import abk.carpark.api.ParkingEntry;
 import abk.carpark.api.ParkingRegister;
 
-public class ParkingRegisterTest {
+public class ParkingRegisterTest
+{
+    
+    private static final String COLOR_BLACK = "Black";
+    private static final String COLOR_WHITE = "White";
 
-	private ParkingRegister parkingRegister;
-	
-	@Rule
-	public ExpectedException e = ExpectedException.none();
+    private static final String REG_NUM_1 = "KA­01­HH­1234";
+    private static final String REG_NUM_2 = "KA­02­HH­1234";
+    private static final String REG_NUM_3 = "KA­03­HH­1234";
 
-	@Before
-	public void setUp() {
-		this.parkingRegister = new ParkingRegister();
-	}
-
-	@Test
-	public void status_empty_returnEmptySet() {
-		Assert.assertEquals(0, parkingRegister.status().length);
-	}
-
-	@Test
-	public void enter_parkTwoCars_statusWithSize2() {
-		parkingRegister.enter(1, "KA­01­HH­1234", "White");
-		parkingRegister.enter(2, "KA­02­HH­1234", "Black");
-		Assert.assertEquals(2, parkingRegister.status().length);
-	}
-	
-	@Test
-	public void exit_exitIllegalCar_throwException()
-	{
-		parkingRegister.enter(1, "KA­01­HH­1234", "White");
-		e.expect(IllegalArgumentException.class);
-		parkingRegister.exit(2);
-	}
-	
-	@Test
-	public void exit_exitCar_statusWithoutThatCar()
-	{
-		parkingRegister.enter(1, "KA­01­HH­1234", "White");
-		parkingRegister.enter(2, "KA­02­HH­1234", "Black");
-		parkingRegister.exit(2);
-		ParkingEntry[] status = parkingRegister.status();
-		ParkingEntry parkingEntry = new ParkingEntry(1, "KA­01­HH­1234", "White");
-		ParkingEntry parkingEntry2 = new ParkingEntry(2, "KA­02­HH­1234", "Black");
-		
-		Assert.assertTrue(Arrays.asList(status).contains(parkingEntry));
-		Assert.assertFalse(Arrays.asList(status).contains(parkingEntry2));
-	}
-	
+    private ParkingRegister parkingRegister;
+    
+    @Rule
+    public ExpectedException e = ExpectedException.none();
+    
+    @Before
+    public void setUp()
+    {
+        this.parkingRegister = new ParkingRegister();
+    }
+    
+    @Test
+    public void status_empty_returnEmptySet()
+    {
+        Assert.assertEquals(0, parkingRegister.status().length);
+    }
+    
+    @Test
+    public void enter_parkTwoCars_statusWithSize2()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_2, COLOR_BLACK);
+        Assert.assertEquals(2, parkingRegister.status().length);
+    }
+    
+    @Test
+    public void exit_exitIllegalCar_throwException()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        e.expect(IllegalArgumentException.class);
+        parkingRegister.exit(2);
+    }
+    
+    @Test
+    public void exit_exitCar_statusWithoutThatCar()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_2, COLOR_BLACK);
+        parkingRegister.exit(2);
+        ParkingEntry[] status = parkingRegister.status();
+        ParkingEntry parkingEntry = new ParkingEntry(1, REG_NUM_1, COLOR_WHITE);
+        ParkingEntry parkingEntry2 = new ParkingEntry(2, REG_NUM_2, COLOR_BLACK);
+        
+        Assert.assertTrue(Arrays.asList(status).contains(parkingEntry));
+        Assert.assertFalse(Arrays.asList(status).contains(parkingEntry2));
+    }
+    
+    @Test
+    public void status_multipleEnterExit_checkConsistentStatus()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_2, COLOR_BLACK);
+        parkingRegister.exit(2);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        ParkingEntry[] status = parkingRegister.status();
+        ParkingEntry parkingEntry = new ParkingEntry(1, REG_NUM_1, COLOR_WHITE);
+        ParkingEntry parkingEntry2 = new ParkingEntry(2, REG_NUM_3, COLOR_WHITE);
+        
+        Assert.assertEquals(2, status.length);
+        Assert.assertTrue(Arrays.asList(status).contains(parkingEntry));
+        Assert.assertTrue(Arrays.asList(status).contains(parkingEntry2));
+    }
+    
+    @Test
+    public void getRegNumsByColor_empty_returnEmptyList()
+    {
+        Assert.assertEquals(0, parkingRegister.getRegNumsByColor(COLOR_WHITE).size());
+    }
+    
+    @Test
+    public void getRegNumsByColor_twoWhiteCars_returnRegNums()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        parkingRegister.enter(3, REG_NUM_2, COLOR_BLACK);
+        List<String> regNums = parkingRegister.getRegNumsByColor(COLOR_WHITE);
+        Assert.assertEquals(2, regNums.size());
+        Assert.assertTrue(regNums.contains(REG_NUM_1));
+        Assert.assertTrue(regNums.contains(REG_NUM_3));
+    }
+    
+    @Test
+    public void getRegNumsByColor_oneBlackCar_returnRegNums()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_BLACK);
+        parkingRegister.enter(2, REG_NUM_2, COLOR_BLACK);
+        parkingRegister.exit(2);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        List<String> regNums = parkingRegister.getRegNumsByColor(COLOR_BLACK);
+        Assert.assertEquals(1, regNums.size());
+        Assert.assertTrue(regNums.contains(REG_NUM_1));
+    }
+    
+    @Test
+    public void getSlotByColor_empty_returnEmptyList()
+    {
+        Assert.assertEquals(0, parkingRegister.getSlotByColor(COLOR_WHITE).size());
+    }
+    
+    @Test
+    public void getSlotByColor_twoWhiteCars_returnSlots()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        parkingRegister.enter(3, REG_NUM_2, COLOR_BLACK);
+        List<Integer> regNums = parkingRegister.getSlotByColor(COLOR_WHITE);
+        Assert.assertEquals(2, regNums.size());
+        Assert.assertTrue(regNums.contains(1));
+        Assert.assertTrue(regNums.contains(2));
+    }
+    
+    @Test
+    public void getSlotByColor_oneBlackCar_returnSlot()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_BLACK);
+        parkingRegister.enter(2, REG_NUM_2, COLOR_BLACK);
+        parkingRegister.exit(2);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        List<Integer> regNums = parkingRegister.getSlotByColor(COLOR_BLACK);
+        Assert.assertEquals(1, regNums.size());
+        Assert.assertTrue(regNums.contains(1));
+    }
+    
+    @Test
+    public void getSlotByRegNum_parkTwo_corresNum()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        int slot = parkingRegister.getSlotByRegNum(REG_NUM_1);
+        Assert.assertEquals(1, slot);
+    }
+    
+    @Test
+    public void getSlotByRegNum_InvalidNum_return0()
+    {
+        parkingRegister.enter(1, REG_NUM_1, COLOR_WHITE);
+        parkingRegister.enter(2, REG_NUM_3, COLOR_WHITE);
+        int slot = parkingRegister.getSlotByRegNum(REG_NUM_2);
+        Assert.assertEquals(0, slot);
+    }
 }
